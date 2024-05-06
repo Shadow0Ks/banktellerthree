642427b4d50bd472a451a479ef30decfffe050d2
ScarletXK <scarletshado0K@gmail.com>
2024-05-06 15:22:17 -0700

    added our names to the project

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ import javax.swing.*;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
+/*
+Mahmoud Alqaisi
+Essam Mashal
+Akhila Herath
+Johnny Ly
+
+ */
 public class Main {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
 
